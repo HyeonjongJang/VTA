cdfa717af3149a6586e4b42d49cc855f7b735381
HyeonjongJang <101302582+HyeonjongJang@users.noreply.github.com>
2025-07-16 11:14:12 +0900

    Update main.py
    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 import streamlit as st
-
-from first_page import *
-from second_page import *
 import os
 
 os.environ["LANGCHAIN_API_KEY"] = st.secrets["LANGCHAIN_API_KEY"]
 os.environ["LANGCHAIN_TRACING_V2"] = 'true'
 os.environ["LANGCHAIN_ENDPOINT"] = "https://api.smith.langchain.com"
 os.environ["LANGCHAIN_PROJECT"] = "Kyung Hee University's Regulations Search Assistant"
+
+from first_page import *
+from second_page import *
 
 def main():
     if "student_id" not in st.session_state:
